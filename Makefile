47fb599748a94964b4d0859a84fa43b4dae02a2d
qinhanlei <qinhanlei@gmail.com>
2016-05-04 17:04:22 +0800

    make it on ubuntu14.04LTS

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
+# CFLAGS = -W -Wall -lsocket -lpthread
+CFLAGS = -W -Wall -lpthread
+
 all: httpd
 
 httpd: httpd.c
-	gcc -W -Wall -lsocket -lpthread -o httpd httpd.c
+	gcc $(CFLAGS) -o httpd httpd.c
 
 clean:
 	rm httpd
--- httpd.c
+++ httpd.c
@@ -22,7 +22,7 @@
 #include <strings.h>
 #include <string.h>
 #include <sys/stat.h>
-#include <pthread.h>
+/*#include <pthread.h>*/
 #include <sys/wait.h>
 #include <stdlib.h>
 
@@ -480,11 +480,11 @@ void unimplemented(int client)
 int main(void)
 {
 	int server_sock = -1;
-	u_short port = 0;
+	u_short port = 9734;
 	int client_sock = -1;
 	struct sockaddr_in client_name;
 	int client_name_len = sizeof(client_name);
-	pthread_t newthread;
+	/*pthread_t newthread;*/
 	
 	server_sock = startup(&port);
 	printf("httpd running on port %d\n", port);
@@ -496,10 +496,10 @@ int main(void)
 		if (client_sock == -1) {
 			error_die("accept");
 		}
-		/* accept_request(client_sock); */
-		if (pthread_create(&newthread , NULL, accept_request, client_sock) != 0) {
+		accept_request(client_sock);
+		/*if (pthread_create(&newthread , NULL, accept_request, client_sock) != 0) {
 			perror("pthread_create");
-		}
+		}*/
 	}
 	
 	close(server_sock);
